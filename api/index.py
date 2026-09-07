f5317c5cd95bf603a33a5f90c7159853ca6b7d15
Larslllllll <lars@skillsmith.ch>
2026-09-07 13:15:40 +0200

    PT-T238 R174: +2 CP (CP=1063); evasion (LOLBins, GTFOBins)

--- api/index.py
+++ api/index.py
@@ -1284,7 +1284,7 @@ _CODE_PATTERNS += [
     (re.compile(r'(?i)Silver.*Ticket|silver.*ticket|service.*ticket.*forge|TGS.*forge'), 9, 'Silver ticket attack'),
     (re.compile(r'(?i)DLL.*Side-Loading|dll.*side.*load|dll.*hijack|search.*order.*hijack'), 8, 'DLL side-loading'),
     (re.compile(r'(?i)LOL.*Driver|lol.*driver|living.*off.*land.*driver|sign.*driver.*vuln'), 7, 'LOL driver abuse'),
-    (re.compile(r'(?i>LOLBins| LOLBins|living.*off.*land.*binaries|microsoft.*signed.*binaries'), 7, 'LOLBins abuse'),
+    (re.compile(r'(?i)LOLBins| LOLBins|living.*off.*land.*binaries|microsoft.*signed.*binaries'), 7, 'LOLBins abuse'),
     (re.compile(r'(?i)GTFOBins| GTFOBins|unix.*binary.*exploit|bin.*suid.*exploit'), 6, 'GTFOBins abuse'),
 ]
 
